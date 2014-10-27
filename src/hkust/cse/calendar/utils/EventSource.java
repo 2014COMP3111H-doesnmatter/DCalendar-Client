59ce251a0c54454a7ead1c1b9048fba786220d19
johnmave126 <johnmave126@gmail.com>
2014-10-27 13:04:34 +0800

    Add fire to one listener functionality.
    Signed-off-by: johnmave126 <johnmave126@gmail.com>

--- src/hkust/cse/calendar/utils/EventSource.java
+++ src/hkust/cse/calendar/utils/EventSource.java
@@ -4,13 +4,29 @@ import java.util.EventObject;
 import java.util.Iterator;
 import java.util.List;
 
+import javax.swing.SwingUtilities;
+
 abstract public class EventSource {
-	final static public <T extends EventObject> void fireList(List<GenListener<T>> list, T e) {
-		Iterator<GenListener<T>> itr = list.iterator();
-		GenListener<T> listener;
-		while(itr.hasNext()) {
-			listener = itr.next();
-			listener.fireEvent(e);
-		}
+	final static public <T extends EventObject> void fireList(final List<GenListener<T>> list, final T e) {
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				Iterator<GenListener<T>> itr = list.iterator();
+				GenListener<T> listener;
+				while(itr.hasNext()) {
+					listener = itr.next();
+					listener.fireEvent(e);
+				}
+			}
+		});
+	}
+	
+	final static public <T extends EventObject> void fireTo(final GenListener<T> listener, final T e) {
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				listener.fireEvent(e);
+			}
+		});
 	}
 }
