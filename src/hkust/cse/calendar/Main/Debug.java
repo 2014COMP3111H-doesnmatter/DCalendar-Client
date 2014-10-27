c4b5f1f399b233fca9ee108904e133ba891ce723
johnmave126 <johnmave126@gmail.com>
2014-10-27 14:35:35 +0800

    Clear Debug.java

--- src/hkust/cse/calendar/Main/Debug.java
+++ src/hkust/cse/calendar/Main/Debug.java
@@ -1,11 +1,5 @@
 package hkust.cse.calendar.Main;
 
-import hkust.cse.calendar.api.welcome.GetActiveUserAPI;
-import hkust.cse.calendar.api.welcome.LoginAPI;
-import hkust.cse.calendar.utils.GenListener;
-import hkust.cse.calendar.utils.network.APIHandler;
-import hkust.cse.calendar.utils.network.APIRequestEvent;
-
 /**
  * 
  * @name Debug
@@ -14,24 +8,6 @@ import hkust.cse.calendar.utils.network.APIRequestEvent;
 public class Debug {
 	static public void main() {
 		try {
-			LoginAPI api1 = new LoginAPI("cirah", "122333");
-			final GetActiveUserAPI api2 = new GetActiveUserAPI();
-			api1.addDoneListener(new GenListener<APIRequestEvent>() {
-				@Override
-				public void fireEvent(APIRequestEvent e) {
-					System.out.println(e.getJSON().toString());
-					Thread thrd2 = new Thread(new APIHandler(api2));
-					thrd2.start();
-				}
-			});
-			api2.addDoneListener(new GenListener<APIRequestEvent>() {
-				@Override
-				public void fireEvent(APIRequestEvent e) {
-					System.out.println(e.getJSON().toString());
-				}
-			});
-			Thread thrd1 = new Thread(new APIHandler(api1));
-			thrd1.start();
 			
 		} catch (Exception e) {
 			e.printStackTrace();
