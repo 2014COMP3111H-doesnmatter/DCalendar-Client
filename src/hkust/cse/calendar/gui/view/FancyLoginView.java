8c3ae96619d73e9aa13e2fe0bf664fff06179ef3
haiyang1992 <haiyang1992@gmail.com>
2014-10-28 11:52:38 +0800

    create class FancyLoginView

---
+++ src/hkust/cse/calendar/gui/view/FancyLoginView.java
@@ -0,0 +1,14 @@
+package hkust.cse.calendar.gui.view;
+
+import hkust.cse.calendar.gui.controller.LoginControllerEvent;
+import hkust.cse.calendar.gui.view.base.BaseLoginView;
+
+public class FancyLoginView extends BaseLoginView {
+
+	@Override
+	public void fireEvent(LoginControllerEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
