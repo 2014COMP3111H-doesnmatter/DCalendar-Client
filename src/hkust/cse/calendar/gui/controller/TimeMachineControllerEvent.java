6a05154b906d346c1e79fbdf69dcbb55a8c49754
ZHAO Liyao <zly0009@gmail.com>
2014-10-30 18:28:06 +0800

    skeleton done

--- src/hkust/cse/calendar/gui/controller/TimeMachineControllerEvent.java
+++ src/hkust/cse/calendar/gui/controller/TimeMachineControllerEvent.java
@@ -6,25 +6,25 @@ import java.util.EventObject;
 
 public class TimeMachineControllerEvent extends EventObject
 {
-	static public final class Command {
-		public static final int START = 0;
+	static public enum Command {
+		START,
 	};
-	private int command;
+	private Command command;
 	
 	public TimeMachineControllerEvent(final Object source) {
 		super(source);
 	}
 	
-	public TimeMachineControllerEvent(final Object source, int command) {
+	public TimeMachineControllerEvent(final Object source, Command command) {
 		super(source);
 		this.command = command;
 	}
 
-	public int getCommand() {
+	public Command getCommand() {
 		return command;
 	}
 
-	public void setCommand(int command) {
+	public void setCommand(Command command) {
 		this.command = command;
 	}
 }
